925c76045a4bf3881db5b744c9a5872551aa72fe
Karina <94340113+e-kari@users.noreply.github.com>
2025-03-16 23:08:42 -0500

    Added responsive queries so that the portfolio displays well throughout different devices.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
       {/* Main Content */}
       <main>
         {/* Hero and About Me Section */}
-        <section className="hero-about-container">
+        <section className="hero-about-container" id="home">
           <section className="hero">
             <h1>Hey there, I'm <span className="pinktext">Kari</span> !</h1>
             <div className="ascii-section">
@@ -36,7 +36,7 @@ const Page = () => {
 
           {/* About Me Section */}
           <section className="about-section" id="about">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <div className="contact">
               <a href="mailto:your-email@example.com">Email</a>
               <a href="https://linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
